7030ab7ffeeba5d4c6020f80b7dd247192434c88
Revem <revemvi@hotmail.com>
2025-01-10 14:28:45 -0300

    fix(CORS): allow all connections via CORS

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from resources.pokemon import blp as PokemonBlueprint
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
     app.config["API_TITLE"] = "Pokedex Abilities REST API"
     app.config["API_VERSION"] = "v1"
